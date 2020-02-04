471a6a9e23ea0c83a50c031e2702c8b99e123d57
Adam Harris <adam_harris@hotmail.com>
2020-02-04 12:18:52 +0000

    0x03: Added player movement

---
+++ Starfighter/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Speed Settings")]
+    [SerializeField] [Tooltip("In ms^-1")] float xSpeed = 4.0f;
+    [SerializeField] [Tooltip("In ms^-1")] float ySpeed = 5f;
+
+    [Header("Player Clamping Settings")]
+    [SerializeField] [Tooltip("In m")] float xClamp = 5f;
+    [SerializeField] [Tooltip("In m")] float yClamp = 3f;
+
+    void Update()
+    {
+        //X Movement
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float newXPos = Mathf.Clamp(rawXPos, -xClamp, xClamp);
+
+        //Y Movement
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float newYPos = Mathf.Clamp(rawYPos, -yClamp, yClamp);
+
+        //Set Position
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+}
